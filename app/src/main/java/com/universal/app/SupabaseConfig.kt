75bc302c148a00ddd88dfcec290bb2741ce45184
GetyeTek <getyeteklu2@gmail.com>
2026-06-23 07:39:34 +0300

    Update Supabase credentials in SupabaseConfig

--- app/src/main/java/com/universal/app/SupabaseConfig.kt
+++ app/src/main/java/com/universal/app/SupabaseConfig.kt
@@ -1,9 +1,9 @@
 package com.universal.app
 
 object SupabaseConfig {
-    const val PROJECT_ID = "vlzgfaqrnyiqfxxxvtas"
+    const val PROJECT_ID = "xvldfsmxskhemkslsbym"
     const val BASE_URL = "https://$PROJECT_ID.supabase.co"
-    const val ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsemdmYXFybnlpcWZ4eHh2dGFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1NTk5NDAsImV4cCI6MjA4MTEzNTk0MH0.y93d68JWyGL7NKXZEHLunAuayMEWw1K6yATFGLxkUxY"
+    const val ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2bGRmc214c2toZW1rc2xzYnltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2ODgxNzksImV4cCI6MjA3ODI2NDE3OX0.5arqrx8Tt7v-hpXpo_ncoK4IX8th9IibxAuv93SSoOU"
 
     const val FUNCTION_URL = "$BASE_URL/functions/v1/upload-image"
     const val STORAGE_URL = "$BASE_URL/storage/v1/object/images/"
